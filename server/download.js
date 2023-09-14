d6cd175c213ade4699d1c205d5338053acf7fa8b
Maycon Padilha <maycon.jordan5000@gmail.com>
2023-09-14 18:37:22 -0300

    segunda aula

---
+++ server/download.js
@@ -0,0 +1,26 @@
+import ytdl from "ytdl-core"
+import fs from "fs"
+import { error } from "console"
+
+export const download = (videoId) => {
+  const videoURL = "https://www.youtube.com/shorts/" + videoId
+  console.log("Realizando o downalod do vídeo:", videoId)
+
+  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+    .on("info", (info) => {
+      const seconds = info.formats[0].approxDurationMs / 1000
+      console.log(seconds)
+
+      if (seconds > 60) {
+        throw new Error("A duração desse video é maior que 60 segundos.")
+      }
+    })
+    .on("end", () => {
+      console.log("Download do video finalizado.")
+    })
+
+    .on("error", () => {
+      console.log("Nao foi possivel fazer o download do video", error)
+    })
+    .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+}
